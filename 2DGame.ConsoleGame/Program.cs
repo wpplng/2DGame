e2a19a07f3eb1e1bf83b156f6dbee0dbf35aee94
wpplng <lina.wappling@gmail.com>
2025-06-03 13:39:57 +0200

    Limited list

--- 2DGame.ConsoleGame/Program.cs
+++ 2DGame.ConsoleGame/Program.cs
@@ -1,4 +1,11 @@
-﻿var game = new Game();
+﻿
+
+using _2DGame.LimitedList;
+
+var lm = new LimitedList<Creature>(10);
+var li = new List<int>(10);
+
+var game = new Game();
 game.Run();
 
 Console.WriteLine("Game has ended. Press any key to exit.");
---
+++ 2DGame.LimitedList/LimitedList.cs
@@ -0,0 +1,22 @@
+﻿namespace _2DGame.LimitedList;
+
+public class LimitedList<T> 
+{
+    private readonly int _capacity;
+    private List<T> _list;
+
+    public int Count => _list.Count;
+    public bool IsFull => _capacity <= Count;
+    public LimitedList(int capacity)
+    {
+        _capacity = Math.Max(capacity, 2);
+        _list = new List<T>(_capacity);
+    }
+
+    public bool Add(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
+        if (IsFull) return false;
+        _list.Add(item); return true;
+    }
+}
