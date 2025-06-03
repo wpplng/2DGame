78eea431a66b5907b999d284e8ead3904a19abe0
wpplng <lina.wappling@gmail.com>
2025-06-03 14:40:07 +0200

    Added Item class

---
+++ 2DGame.ConsoleGame/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DGame.ConsoleGame
+{
+    internal class Item : IDrawable
+    {
+        private readonly string _name;
+        public ConsoleColor Color { get; }
+
+        public string Symbol { get; }
+
+        public Item(string symbol, ConsoleColor color, string name)
+        {
+
+            Symbol = symbol;
+            Color = color;
+            _name = name;
+        }
+        public override string ToString() => _name;
+
+    }
+}
